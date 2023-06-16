640c35913e3d17984d5dcdbf32f76ae2b4e86648
Alan Lopez <99952929+AlanLopRey@users.noreply.github.com>
2023-06-15 20:12:48 -0600

    Finish the menu hamburger and add features to the nav
    finish the menu with the navs inside
    -add a efect for the navigation bar a mix blend mode and the efect on
    the scroll

---
+++ menu.js
@@ -0,0 +1,35 @@
+const d = document;
+function hamburgerMenu(menuBtn, hamburgerMenu) {
+  d.addEventListener("click", (e) => {
+    //si le hacemos click a el panel btn o todo lo que este dentro de el panel btn
+    if (e.target.matches(menuBtn) || e.target.matches(`${menuBtn} *`)) {
+      //entonces a la clase panle activale o desactivale la clase is-active
+      d.querySelector(hamburgerMenu).classList.toggle("is-active");
+      //y a la clase panelBtn tambien le agregamos o quitamos el is-active
+      d.querySelector(menuBtn).classList.toggle("is-active");
+    }
+  });
+}
+
+hamburgerMenu(".menu-btn", ".menu");
+
+//Navbar control
+
+const navbar = d.querySelector("#navbar");
+
+console.log(navbar);
+let lastScrollTop = 0;
+
+window.addEventListener(
+  "scroll",
+  (e) => {
+    var { pageYOffset } = window;
+    if (pageYOffset > lastScrollTop && pageYOffset >= 120) {
+      navbar.classList.remove("visible");
+    } else if (pageYOffset < lastScrollTop) {
+      navbar.classList.add("visible");
+    }
+    lastScrollTop = pageYOffset <= 0 ? 0 : pageYOffset;
+  },
+  { passive: true }
+);
