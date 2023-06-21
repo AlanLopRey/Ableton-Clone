74543631177bed249ef02797b26f9c0aafc35f5b
Alan Lopez <99952929+AlanLopRey@users.noreply.github.com>
2023-06-20 22:14:38 -0600

    Finish the headear with the hamburger menu and footer
    -add some details to the footer
    -add the javascript for the triangle in the header menu nav

--- menu.js
+++ menu.js
@@ -1,17 +1,18 @@
 const d = document;
-function hamburgerMenu(menuBtn, hamburgerMenu) {
+function hamburgerMenu(menuBtn, hamburgerMenu, menuTriangle) {
   d.addEventListener("click", (e) => {
     //si le hacemos click a el panel btn o todo lo que este dentro de el panel btn
     if (e.target.matches(menuBtn) || e.target.matches(`${menuBtn} *`)) {
       //entonces a la clase panle activale o desactivale la clase is-active
       d.querySelector(hamburgerMenu).classList.toggle("is-active");
+      d.querySelector(menuTriangle).classList.toggle("rotate-triangle");
       //y a la clase panelBtn tambien le agregamos o quitamos el is-active
       d.querySelector(menuBtn).classList.toggle("is-active");
     }
   });
 }
 
-hamburgerMenu(".menu-btn", ".menu");
+hamburgerMenu(".menu-btn", ".menu", ".menu-triangle");
 
 //Navbar control
 
